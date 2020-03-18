e6659e4fe111d3f155cf1203259d98153e9e6b33
Sebastian Wålinder <s.walinder@gmail.com>
2020-03-19 00:41:27 +0100

    Refactored rendering

--- src/FRPEngine/Render/SDL/Primitives.hs
+++ src/FRPEngine/Render/SDL/Primitives.hs
@@ -19,9 +19,9 @@ import SDL.Vect
 import SDL.Video.Renderer
 
 screenSize :: (Num a) => V2 a
-screenSize = V2 1280 720
+-- screenSize = V2 1280 720
 -- screenSize = V2 2560 1440
--- screenSize = V2 1920 1080
+screenSize = V2 1920 1080
 
 copyEx' :: MonadIO m => Renderer -> Texture -> Maybe (Rectangle CInt) -> V2 CInt -> V2 CInt -> CDouble -> Maybe (Point V2 CInt) -> V2 Bool -> m ()
 copyEx' rend spr sourceRect pos destRect =
@@ -33,33 +33,42 @@ copyEx' rend spr sourceRect pos destRect =
 {-# INLINE copyEx' #-}
 
 renderEx' :: (RealFrac a, MonadIO m) => Renderer -> Maybe (V2 a) -> Texture -> V2 a -> V2 a -> a -> Bool -> a -> V2 a -> m ()
-renderEx' rend rotCenter spr pos size' theta renderFromCenter zoom playerPos =
+renderEx' rend rotCenter spr _pos size theta renderFromCenter zoom _camPos =
   copyEx'
     rend
     spr
     Nothing
     -- Pos of object
-    (fmap floor ((pos' + (screenMiddle - negateYAxis playerPos)) / pure zoom))
-    (fmap floor (size' / pure zoom))
+    (fmap floor ((pos + (screenMiddle - camPos)) / pure zoom))
+    (fmap floor (size / pure zoom))
     (realToFrac theta)
     (rotCenter >>= (\center -> Just $ P $ fmap floor $ center / pure zoom))
     (V2 False False)
   where
-    -- Set pos depending on if rendering should be done from center or not
-    pos' =
+    camPos = negateYAxis _camPos
+    -- SDL renders from top left.. This offsets all game state so that it acts as if bottom left rendering was used
+    pos =
       negateYAxis
-        ( case renderFromCenter of
-            True -> pos - negateYAxis (size' / 2)
-            False -> pos
+        ( _pos
+            +
+            -- Some objects you want to render from center
+            ( case renderFromCenter of
+                -- Objects that render from center needs to be moved half its size up and half its size right
+                True -> negateXAxis size / 2
+                -- Objects that render from bottom left needs to move down its size
+                False -> V2 0 (size ^. _y)
+            )
         )
     screenMiddle = (screenSize / 2) * pure zoom
     negateYAxis :: (Num a) => V2 a -> V2 a
     negateYAxis = _y `over` negate
+    negateXAxis :: (Num a) => V2 a -> V2 a
+    negateXAxis = _x `over` negate
 {-# INLINE renderEx' #-}
 
 -- Objects that don't render from center instead render from bottom left
 renderObj :: (RealFrac a) => V2 a -> (Object a w -> S.Texture) -> a -> S.Renderer -> Bool -> Object a w -> IO ()
-renderObj playerPos res zoomLevel renderer renderFromCenter obj =
+renderObj camPos res zoomLevel renderer renderFromCenter obj =
   renderEx'
     renderer
     Nothing
@@ -69,7 +78,7 @@ renderObj playerPos res zoomLevel renderer renderFromCenter obj =
     (obj ^. rot)
     renderFromCenter
     zoomLevel
-    playerPos
+    camPos
 {-# INLINE renderObj #-}
 
 renderText renderer font text pos = do
@@ -83,12 +92,12 @@ renderText renderer font text pos = do
 {-# INLINE renderText #-}
 
 renderLine :: (MonadIO m) => Renderer -> V2 CInt -> V2 CInt -> [[V2 CInt]] -> m ()
-renderLine rend playerPos zoomLevel points =
+renderLine rend camPos zoomLevel points =
   drawLines rend points'
   where
     v2Div = liftA2 div
     points' =
       fromList $
         P
-          <$> (fmap (+ playerPos `v2Div` zoomLevel) (join points))
+          <$> (fmap (+ camPos `v2Div` zoomLevel) (join points))
 {-# INLINE renderLine #-}
